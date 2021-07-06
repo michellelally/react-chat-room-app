14eef4e35e9532fc841417dda1ad53109bbcdb36
michellelally <shelley.lally@gmail.com>
2021-07-06 19:45:36 +0100

    firebase configured

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
+firebase.initializeApp({
+  apiKey: "AIzaSyC79wGmZB4dl7MRdBizB-yZogmcZLkgjWE",
+  authDomain: "react-chat-app-9dbf7.firebaseapp.com",
+  projectId: "react-chat-app-9dbf7",
+  storageBucket: "react-chat-app-9dbf7.appspot.com",
+  messagingSenderId: "774707000046",
+  appId: "1:774707000046:web:de486b4d69e3b5df8d79ff"
+
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 
 function App() {
