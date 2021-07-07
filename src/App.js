317d367840d2bce70d936b763c6d310dfec11d7d
michellelally <shelley.lally@gmail.com>
2021-07-07 11:14:30 +0100

    testing deployment on herkou

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useState } from 'react';
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -32,7 +32,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
- 
+        <h1>React Chat</h1>
+        <SignOut />
       </header>
 
       <section>
@@ -54,19 +55,22 @@ function SignIn() {
   }
 
   return (
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
   )
 }
 
 function SignOut() {
   return auth.currentUser && (
 
-    <button onClick={ () => auth.signOut}>Sign Out</button>
+    <button className="sign-out" onClick={ () => auth.signOut}>Sign Out</button>
 
   )
 }
 
 function ChatRoom() {
+
+  const dummy = useRef(); 
+
   // querying a subset of documents called messages
   const messagesRef = firestore.collection('messages');
   // ordering them by the time they were created at lmiting at 25
@@ -77,14 +81,44 @@ function ChatRoom() {
   // each object is the chat message in the database
   const [messages] = useCollectionData(query, {idField: 'id'});
 
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+
+    e.preventDefault();
+
+    const { uid, photoURL} = auth.currentUser; 
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+
+    dummy.current.scrollIntoView({ behavior: 'smooth'});
+
+  }
+
   return (
     <>
     <div>
+      <div>
       {/*
         mapping over the array of messages using a ChatMessage component for each message
         it passes the key prop with the value of id and document data as a message prop
       */}
       {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} /> )}
+      </div>
+      <div ref={dummy}></div>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+        <button type="submit">💩</button>
+      </form>
+
     </div>
     </>
   )
@@ -93,6 +127,19 @@ function ChatRoom() {
 
 function ChatMessage(props) {
 
+  const { text, uid, photoURL } = props.message;
+  // distinguish between sent and recieved messages 
+  // by comparing the user id on firestore document with the logged in user
+  const messageClass = uid === auth.currentUser.uid;
+
+return (
+  <div className={'message ${messageClass}'}>
+    <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+    <p>{text}</p>
+
+  </div>
+
+)
 }
 
 export default App;
